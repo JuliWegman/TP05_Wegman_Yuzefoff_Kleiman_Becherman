9565c8c4af178f4739fde5a8b04db10188d40aa2
JULIÁN WEGMAN <47806018@comunidadort.edu.ar>
2023-06-07 09:07:13 -0300

    cuarto commit

--- Models/Escape.cs
+++ Models/Escape.cs
@@ -6,9 +6,9 @@ public static int estadoJuego{get;set;}=0;
 public static void InicializarJuego(){
 incognitasSalas[0]="0112358";
 incognitasSalas[1]=".x...x.";
-incognitasSalas[2]="";
+incognitasSalas[2]="TOPO";
 incognitasSalas[3]="";
-incognitasSalas[4]="19043";
+incognitasSalas[4]="1904";
 }
 public static bool ResolverSala(string Incognita)
 {
